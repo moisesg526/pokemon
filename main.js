3b528fe6bec53490413924ef702b23c829294798
moises gomez <moisesg526@yahoo.com>
2023-02-15 00:04:34 -0500

    Fixing pokemon image

--- main.js
+++ main.js
@@ -6,14 +6,24 @@ function getPokemon(e) {
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
-      const pokemon_img = document.getElementById("pokemon_img").innerHTML = data.sprites.front_default;
-      pokemon_img.src =  data.sprites.front_default;
+      
       document.getElementById("data_id").innerHTML = data.id;
       document.getElementById("data_name").innerHTML = data.name;
       document.getElementById("data_type").innerHTML = data.types
         .map((type) => type.type.name)
         .join(", ");
-      document.getElementById("data_moves").innerHTML = data.moves.map((move) => move.move.name).slice(0, 5).join(", ");
+      document.getElementById("data_moves").innerHTML = data.moves
+        .map((move) => move.move.name)
+        .slice(0, 5)
+        .join(", ");
+
+        document.getElementById("search").addEventListener("click", getImage);
+          
+        function getImage() {
+            const img = new Image();
+            img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${name || id}.png`;
+            document.getElementById("image").appendChild(img);
+        }
     })
     .catch((err) => {
       console.log("Error pokemon not found", err);
